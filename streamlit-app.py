1edeb8c5a79c25ceb47def394dfd2a231c4a8256
bilalakram777 <161134847+bilalakram777@users.noreply.github.com>
2024-10-01 15:26:03 +0500

    streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,14 +1,8 @@
+import streamlit as stimport pickle
+import pandas as pd
+import requests
+import joblib
 
-try:
-    import streamlit as st
-    import pickle
-    import pandas as pd
-    import requests
-    import joblib
-except ModuleNotFoundError as e:
-    print(f"Error: {e}")
-    st.error(f"Error: {e}")
-    st.stop()
 
 def fetch_movie_id(movie_title):
     response = requests.get("https://api.themoviedb.org/3/search/movie?api_key=2eec5cadfef931cf08e9b0805fe12e88&query={}".format(movie_title))
